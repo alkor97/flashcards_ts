24613beb7829d21a66439fe6cff919d20f46fd68
Maciej Stachurski <alkor@konto.pl>
2024-03-04 21:56:19 +0100

    Dead code removed.

--- src/js/comparators.test.ts
+++ src/js/comparators.test.ts
@@ -1,13 +1,6 @@
-import { isEqual, distance } from "./comparators";
+import { distance } from "./comparators";
 
 describe("verify comparators", () => {
-  test("only plain letters", () => {
-    expect(isEqual("abc", "abc")).toBeTruthy();
-    expect(isEqual("abc", "cba")).toBeFalsy();
-  });
-  test("accented letters", () => {
-    expect(isEqual("ábc", "abc", "es")).toBeTruthy();
-  });
   test("word distance", () => {
     expect(distance("kitten", "sitting")).toBe(3);
   });
--- src/js/comparators.ts
+++ src/js/comparators.ts
@@ -1,45 +1,5 @@
 import levenshtein from "js-levenshtein";
 
-interface Rule {
-  from: string;
-  to: string;
-}
-
-interface Replacement {
-  regex: RegExp;
-  value: string;
-}
-
-function createReplacements(r: Rule): Replacement[] {
-  const minLength = Math.min(r.from.length, r.to.length);
-  return Array.from({ length: minLength }, (_, i) => {
-    const regex = new RegExp(r.from.charAt(i), "g");
-    const value = r.to.charAt(i);
-    return { regex, value };
-  });
-}
-
-const replacementsMap = new Map<string, Replacement[]>();
-replacementsMap.set("es", createReplacements({
-  from: "áéíóúýÁÉÍÓÚÝñÑ",
-  to: "aeiouyAEIOUYnN",
-}));
-
-function commonize(text: string, replacements: Replacement[]): string {
-  const combinedRegex = new RegExp(replacements.map(r => r.regex.source).join('|'), 'g');
-  const replacementDict = Object.fromEntries(replacements.map(r => [r.regex.source, r.value]));
-  return text.replace(combinedRegex, match => replacementDict[match]);
-}
-
-function getReplacements(locale?: string): Replacement[] {
-  return replacementsMap.get((locale || "").toLowerCase()) || [];
-}
-
-export function isEqual(a: string, b: string, locale?: string): boolean {
-  const replacements = getReplacements(locale);
-  return commonize(a, replacements) === commonize(b, replacements);
-}
-
 export function distance(a: string, b: string): number {
   return levenshtein(a, b);
 }
