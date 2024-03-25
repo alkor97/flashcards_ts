37461d30ef74cce5e15967fd92e8ccc2d62099c9
Maciej Stachurski <alkor@konto.pl>
2024-03-25 19:25:36 +0100

    Colors as CSS variables.

--- src/js/multiple-match/index.ts
+++ src/js/multiple-match/index.ts
@@ -1,245 +1,12 @@
-import { DataEntry, toDataEntry, withTagsIncrementally } from "../parsing/repository";
-import { shuffle, randomFrom } from "../utils/random";
+import {
+  DataEntry,
+  toDataEntry,
+  withTagsIncrementally,
+} from "../parsing/repository";
 import { parseTsv } from "../parsing/parser";
-
-class Element {
-  constructor(
-    public readonly id: number,
-    public readonly text: string,
-    public readonly matchingId: number,
-    public selected: boolean = false,
-    public empty: boolean = false
-  ) {}
-
-  public matches(other: Element): boolean {
-    return this.id === other.matchingId && this.matchingId === other.id;
-  }
-}
-
-type IdGenerator = () => number;
-
-function toElements(entry: DataEntry, nextId: IdGenerator): [Element, Element] {
-  const leftId = nextId();
-  const rightId = nextId();
-  return [
-    new Element(leftId, entry.key, rightId),
-    new Element(rightId, entry.value, leftId),
-  ];
-}
-
-interface Selection {
-  index: number;
-  selected: boolean;
-}
-
-class Model {
-  public readonly left: Element[] = [];
-  public readonly right: Element[] = [];
-  private generator = 0;
-  private matchedPairs = 0;
-
-  constructor(public readonly total: number) {}
-
-  public add(entry: DataEntry) {
-    const columns = [this.left, this.right];
-    toElements(entry, () => this.generator++).forEach((value, index) =>
-      columns[index].push(value)
-    );
-  }
-
-  public select(column: Column, index: number) {
-    const result: Selection[] = [];
-    const elements = column === Column.LEFT ? this.left : this.right;
-    const deselectedIndex = elements.findIndex(
-      (e, i) => e.selected && i !== index
-    );
-    if (deselectedIndex >= 0 && !elements[deselectedIndex].empty) {
-      elements[deselectedIndex].selected = false;
-      result.push({
-        index: deselectedIndex,
-        selected: false,
-      });
-    }
-    if (index < elements.length && !elements[index].empty) {
-      elements[index].selected = true;
-      result.push({
-        index,
-        selected: true,
-      });
-    }
-    return result;
-  }
-
-  public shuffleColumn(column?: Column) {
-    shuffle(
-      column === Column.LEFT
-        ? this.left
-        : column === Column.RIGHT
-        ? this.right
-        : randomFrom([this.left, this.right])
-    );
-  }
-
-  public challengeSelection(): [number, number, boolean] | undefined {
-    const leftSelectedIndex = this.left.findIndex((e) => e.selected);
-    if (leftSelectedIndex >= 0) {
-      const rightSelectedIndex = this.right.findIndex((e) => e.selected);
-      if (rightSelectedIndex >= 0) {
-        const matched = this.left[leftSelectedIndex].matches(
-          this.right[rightSelectedIndex]
-        );
-        if (matched) {
-          this.matchedPairs++;
-        }
-        return [leftSelectedIndex, rightSelectedIndex, matched];
-      }
-    }
-    return undefined;
-  }
-
-  public resetSelection(leftIndex: number, rightIndex: number) {
-    this.left[leftIndex].selected = false;
-    this.right[rightIndex].selected = false;
-  }
-
-  public removeEmpty() {
-    function impl(data: Element[]) {
-      let i = data.length;
-      while (i--) {
-        if (data[i].empty) {
-          data.splice(i, 1);
-        }
-      }
-    }
-    impl(this.left);
-    impl(this.right);
-  }
-
-  public markEmpty(leftIndex: number, rightIndex: number) {
-    this.left[leftIndex].empty = true;
-    this.right[rightIndex].empty = true;
-  }
-
-  public get length() {
-    return Math.min(this.left.length, this.right.length);
-  }
-
-  public get matched() {
-    return this.matchedPairs;
-  }
-}
-
-function toMillis(text: string): number {
-  const multiplier = text.endsWith("ms") ? 1 : 1000;
-  return parseFloat(text) * multiplier;
-}
-
-type ViewElementState = "normal" | "selected" | "wrong" | "valid" | "collapsed";
-
-class ViewElement {
-  public readonly view = document.createElement("div");
-  constructor() {
-    this.view.classList.add("content");
-  }
-  public set state(value: ViewElementState) {
-    this.view.setAttribute("state", value);
-  }
-  public set content(value: string) {
-    this.view.textContent = value;
-  }
-  public addClickHandler(handler: () => void) {
-    this.view.addEventListener("click", handler);
-  }
-  public get transitionDuration(): number {
-    return toMillis(getComputedStyle(this.view).transitionDuration);
-  }
-}
-
-class ViewColumn {
-  private readonly elements: ViewElement[] = [];
-  constructor(private anchor: HTMLElement) {}
-  public addElement(handler: (index: number) => void): ViewElement {
-    const entry = new ViewElement();
-    entry.content = " ";
-    const size = this.elements.length;
-    entry.addClickHandler(() => handler(size));
-    this.elements.push(entry);
-    this.anchor.appendChild(entry.view);
-    return entry;
-  }
-  public setState(index: number, state: ViewElementState) {
-    this.byIndex(index).state = state;
-  }
-  public setContent(index: number, content: string) {
-    this.byIndex(index).content = content;
-  }
-  public getTransitionDuration(index: number): number {
-    return this.byIndex(index).transitionDuration;
-  }
-  private byIndex(index: number): ViewElement {
-    return this.elements[index];
-  }
-  public get length() {
-    return this.elements.length;
-  }
-}
-
-enum Column {
-  LEFT,
-  RIGHT,
-}
-
-type ClickHandler = (column: Column, index: number) => void;
-
-class View {
-  private handler: ClickHandler = () => {};
-  private readonly progress = document.querySelector("progress");
-
-  constructor(private left: ViewColumn, private right: ViewColumn) {
-    const body = document.body;
-    let previousHeight = body.clientHeight;
-    let step = 0;
-    // create enough rows to fill the screen vertically
-    while (body.clientHeight + step < window.innerHeight) {
-      this.left.addElement((index) => this.handler(Column.LEFT, index));
-      this.right.addElement((index) => this.handler(Column.RIGHT, index));
-      step = Math.max(step, body.clientHeight - previousHeight);
-      previousHeight = body.clientHeight;
-    }
-  }
-
-  public setState(column: Column, index: number, state: ViewElementState) {
-    this.byColumn(column).setState(index, state);
-  }
-
-  public setContent(column: Column, index: number, content: string) {
-    this.byColumn(column).setContent(index, content);
-  }
-
-  public getTransitionDuration(column: Column, index: number): number {
-    return this.byColumn(column).getTransitionDuration(index);
-  }
-
-  private byColumn(column: Column): ViewColumn {
-    return column === Column.LEFT ? this.left : this.right;
-  }
-
-  public get length() {
-    return Math.min(this.left.length, this.right.length);
-  }
-
-  public set clickHandler(value: ClickHandler) {
-    this.handler = value;
-  }
-
-  public setProgress(current: number, total: number) {
-    if (this.progress && current >= 0 && total > 0) {
-      const percentage = Math.round((100 * current) / total);
-      this.progress.value = percentage;
-      this.progress.max = 100;
-    }
-  }
-}
+import { Model, Element } from "./model";
+import { View, ViewElementState, ViewColumn } from "./view";
+import { Column } from "./common";
 
 function sleep(millis?: number) {
   return new Promise((resolve) => setTimeout(resolve, millis));
@@ -287,8 +54,7 @@ class ViewModel {
 
         await sleep(view.getTransitionDuration(Column.LEFT, leftIndex));
         if (valid) {
-          model.markEmpty(leftIndex, rightIndex);
-          model.removeEmpty();
+          model.remove(leftIndex, rightIndex);
 
           if (model.length > 0) {
             // do not set normal state if the model is empty to prevent showing last matched pair
@@ -340,14 +106,10 @@ class ViewModel {
       [Column.RIGHT, this.model.right],
     ];
     for (let i = 0; i < this.view.length; ++i) {
-      // reflect model into target
+      // reflect model into view
       targets.forEach(([column, model]) => {
         this.view.setState(column, i, "normal");
-        this.view.setContent(
-          column,
-          i,
-          i < model.length ? (model[i].empty ? " " : model[i].text) : ""
-        );
+        this.view.setContent(column, i, i < model.length ? model[i].text : "");
       });
     }
   }
@@ -359,12 +121,9 @@ async function runOnInit() {
   const result = await fetch("./pol-esp.tsv");
   const text = await result.text();
   const params = new URLSearchParams(window.location.search);
-  const data = shuffle(
-    parseTsv(text)
-      .map(toDataEntry)
-      .filter(withTagsIncrementally(params.getAll("tags")))
-  );
-
+  const data = parseTsv(text)
+    .map(toDataEntry)
+    .filter(withTagsIncrementally(params.getAll("tags")));
   const model = new Model(data.length);
   const view = new View(
     new ViewColumn(document.querySelector<HTMLElement>(".left")!),
